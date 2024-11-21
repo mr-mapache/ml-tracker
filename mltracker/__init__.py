d9f4effebb8c87ee8235c5dc12fec428557f572b
Password <eric.m.cardozo@mi.unc.edu.ar>
2024-11-21 00:10:17 -0300

    Update __init__.py

--- mltracker/__init__.py
+++ mltracker/__init__.py
@@ -1,6 +1,6 @@
 from mltracker.adapters.tinydb.getters import get_experiments_collection, get_experiment, get_aggregates_collection
 from mltracker.adapters.tinydb.experiments import Experiment
-from mltracker.adapters.tinydb.aggregates import Aggregate
+from mltracker.adapters.tinydb.aggregates import Aggregate, Aggregates
 from mltracker.adapters.tinydb.metrics import Metric
 from mltracker.adapters.tinydb.iterations import Iteration
 from mltracker.adapters.tinydb.aggregates import Module
